446b8764b2aad591770f880da670d3caeb25b172
prkgnt <pgt258258@gmail.com>
2024-11-07 05:15:01 +0000

    See https://github.com/no-commit-today/dev-a-nice-day-front/commit/ce61ab0396451e0a72d7a69131f9203d3f41713a from refs/heads/develop

--- src/app/_components/LoginModal.tsx
+++ src/app/_components/LoginModal.tsx
@@ -25,7 +25,7 @@ export default function LoginModal({
             className={styles.githubButton}
             onClick={() =>
               router.push(
-                "https://github.com/login/oauth/authorize?client_id=Iv23lipZn6Q52xQOthNr"
+                `https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_GITHUB_REDIRECT_URI}`
               )
             }
           >
